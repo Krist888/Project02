2a4f296198b708633e426a04dfb9d8cc59b35b29
Krist888 <865026703@qq.com>
2020-10-11 19:29:47 +0800

    Initial commit

---
+++ Module01/src/com/huawei/bean/Customer.java
@@ -0,0 +1,35 @@
+package com.huawei.bean;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+//这是单行注释
+public class Customer {
+    public static void main(String[] args){
+        System.out.println("HelloWorld!");
+        add();
+int a = 1;
+        System.out.println("Customer.main");
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("Customer.main");
+        System.out.println("a = " + a);
+
+
+
+
+    }
+
+    public static void add() {
+
+        ArrayList arrayList = new ArrayList();
+        Date date = new Date();
+    }
+
+    /*这是多行注释*/
+            public void method(){
+
+            }}
+
+
---
+++ src/com/huawei/java/Dome.java
@@ -0,0 +1,26 @@
+package com.huawei.java;
+
+/**
+ * @author Krist
+ * @date 2020/10/11 -9:27
+ */
+public class Dome {
+    public static void main(String[] args) {
+
+    }
+
+    public void testAdd(){
+        
+    }
+    /**
+     * 顾客的姓名name
+     */
+    //prsc 
+    private String name;
+     //pric
+    /**
+     * 顾客的id
+     */
+     private int id = 1;
+     
+}
